838e9bd79575b41c1a005a94894d8f34f26d2b29
langhat <3662000756@qq.com>
2026-03-25 13:46:11 +0800

    上传!!!

---
+++ src/neuron.cpp
@@ -0,0 +1,70 @@
+#ifndef NEURON_CPP
+#define NEURON_CPP
+
+#include<vector>
+
+using real = long double;
+
+class neuron {
+public:
+	virtual real count() =0;
+}
+
+class norneuron: public neuron {
+protected:
+	std::vector<neuron*> source;
+	std::vector<real>    ws;
+public:
+	norneuron() {
+
+	}
+	virtual real active(real in) {
+		return (in > 0) ? in : 0;
+	}
+	virtual real count() {
+		real result;int index=0;
+		for(const auto &ptr: source) {
+			if(ptr) {
+				result += *ptr.count() * ws[index];
+			}
+			index++;
+		}
+		return active(result);
+	}
+
+}
+
+class rneuron: public norneuron {
+	real store;
+public:
+	real forget(real memory) {
+	    return 1.0 / (1.0 + exp(-memory));
+	}
+	real count() {
+		real result;int index=0;
+		for(const auto &ptr: source) {
+			if(ptr) {
+				result += ptr->count() * ws[index];
+			}
+			index++;
+		}
+		result = store * ws[index];
+		store = active(result);
+
+		return store = active(result);
+	}
+}
+
+class input: public neuron {
+	real inside;
+public:
+	input (real inv = 0.0): inside(inv) {}
+	real count() {
+		return inside;
+	}
+	void set_inside(real inv) {
+		inside = inv;
+	}
+}
+
+#endif
